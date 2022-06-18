bb53e0c0367626b3030ec29f14ddc3ec0ff561a0
bamboo-cloud <18131124226@163.com>
2022-06-18 11:35:27 +0800

    create gittest

---
+++ src/main/java/com/bamboo/Test.java
@@ -0,0 +1,12 @@
+package com.bamboo;
+
+/**
+ * @param
+ * @author Bamboo
+ * @since JDK 1.8
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
